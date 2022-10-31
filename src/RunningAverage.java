0d7ea5637d0ffa8bd1988a8ef4ce2f1793d7a9d6
BT_4N2_24 <rkong2588@bths.edu>
2022-10-31 12:30:41 -0400

    finished RunningAverage

---
+++ src/RunningAverage.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+public class RunningAverage {
+    public static void main(String[] args) {
+        // Instance Variables
+        Scanner s = new Scanner(System.in);
+        int total = 0;
+        int userInput = 0;
+        int sum = 0;
+
+        System.out.print("Enter a number (or -1 to quit): ");
+        userInput = s.nextInt();
+        if (userInput != -1)
+        {
+            total++;
+            sum = userInput + sum;
+        }
+
+        while (userInput != -1)
+        {
+            System.out.print("Enter a number (or -1 to quit): ");
+            userInput = s.nextInt();
+            if (userInput != -1)
+            {
+                total++;
+                sum = userInput + sum;
+            }
+        }
+
+        if (total == 0)
+        {
+            System.out.println("You entered no numbers so nothing to show!");
+        }
+        else
+        {
+            System.out.println("Sum: " + sum);
+            System.out.println("Number of numbers added: " + total);
+            System.out.println("Average: " + (sum / total));
+        }
+    }
+}
